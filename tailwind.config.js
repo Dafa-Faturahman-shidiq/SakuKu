a1a42145073e03b80d5b1945d1f5ecfd1c8e1a0b
dafa <dafaxpplg3@gmail.com>
2025-06-14 14:46:46 +0700

    menambahkan icon pada project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./dist/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+// tailwind.config.js
+const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons');
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // atau sesuaikan dengan lokasi file HTML kamu
+    './dist/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(['material-symbols']),
+    }),
+  ],
+};
